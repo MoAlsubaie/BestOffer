a0492df5fe4d1d46f73c13eae1e72a507847d523
muhmmedAlsubaie <35927342+muhmmedAlsubaie@users.noreply.github.com>
2018-03-18 13:08:03 +0300

    Search update

--- app/src/main/java/bestoffer/kau/edu/bestoffer/GridAdapter.java
+++ app/src/main/java/bestoffer/kau/edu/bestoffer/GridAdapter.java
@@ -11,28 +11,31 @@ import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
 public class GridAdapter extends BaseAdapter {
 
     private Context mContext;
+    ArrayList<items> Ar ;
 
-    public GridAdapter(Context context) {
+    public GridAdapter(Context context , ArrayList<items> Ar ) {
         mContext = context;
-
+        this.Ar = Ar ;
     }
 
     @Override
     public int getCount() {
-        return items.Ar.size();
+        return Ar.size();
     }
 
     @Override
     public Object getItem(int i) {
-        return items.Ar.get(i);
+        return Ar.get(i);
     }
 
     @Override
     public long getItemId(int i) {
-        return items.Ar.get(i).getId();
+        return Ar.get(i).getId();
     }
 
     @Override
@@ -62,22 +65,22 @@ public class GridAdapter extends BaseAdapter {
         TextView textViewAndroidO = (TextView) gridViewAndroid.findViewById(R.id.gridview_priceold);
         ImageView imageViewAndroidLogo = (ImageView) gridViewAndroid.findViewById(R.id.gridview_logo);
 
-        textViewAndroid.setText(items.Ar.get(i).getName());
-        new GetImg(imageViewAndroid , i).execute(items.Ar.get(i).getPictureLink()) ;
-        if(items.Ar.get(i).getSupermarket().equalsIgnoreCase("ca")){
+        textViewAndroid.setText(Ar.get(i).getName());
+        new GetImg(imageViewAndroid , i).execute(Ar.get(i).getPictureLink()) ;
+        if(Ar.get(i).getSupermarket().equalsIgnoreCase("ca")){
             imageViewAndroidLogo.setImageResource(R.drawable.ca);
-        }else if(items.Ar.get(i).getSupermarket().equalsIgnoreCase("da")){
+        }else if(Ar.get(i).getSupermarket().equalsIgnoreCase("da")){
             imageViewAndroidLogo.setImageResource(R.drawable.da);
         }else{
             imageViewAndroidLogo.setImageResource(R.drawable.pa);
         }
 
-        if(items.Ar.get(i).getOffer() != 0){
-            textViewAndroidO.setText(String.valueOf(items.Ar.get(i).getPrice()));
-            textViewAndroidP.setText(String.valueOf(items.Ar.get(i).getOffer()));
+        if(Ar.get(i).getOffer() != 0){
+            textViewAndroidO.setText(String.valueOf(Ar.get(i).getPrice()));
+            textViewAndroidP.setText(String.valueOf(Ar.get(i).getOffer()));
             textViewAndroidO.setPaintFlags(textViewAndroidO.getPaintFlags() | Paint.STRIKE_THRU_TEXT_FLAG);
         }else{
-            textViewAndroidP.setText(String.valueOf(items.Ar.get(i).getPrice()));
+            textViewAndroidP.setText(String.valueOf(Ar.get(i).getPrice()));
             textViewAndroidO.setText("");
 
         }
--- app/src/main/java/bestoffer/kau/edu/bestoffer/browseActivity.java
+++ app/src/main/java/bestoffer/kau/edu/bestoffer/browseActivity.java
@@ -2,16 +2,27 @@ package bestoffer.kau.edu.bestoffer;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.GridView;
 import android.widget.Toast;
 
+import com.miguelcatalan.materialsearchview.MaterialSearchView;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
 public class browseActivity extends AppCompatActivity  {
     GridView androidGridView;
      Context context = this ;
+    MaterialSearchView searchView ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,6 +31,77 @@ public class browseActivity extends AppCompatActivity  {
 
         Toast.makeText(this, "updateing...", Toast.LENGTH_SHORT).show();
 
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar) ;
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Best Offer");
+        toolbar.setTitleTextColor(Color.parseColor("#FFFFFF"));
+        br(items.Ar);
+
+         searchView = (MaterialSearchView)findViewById(R.id.search_view);
+
+        searchView.setOnSearchViewListener(new MaterialSearchView.SearchViewListener() {
+            @Override
+            public void onSearchViewShown() {
+
+            }
+
+            @Override
+            public void onSearchViewClosed() {
+                br(items.Ar);
+
+            }
+        });
+
+        searchView.setOnQueryTextListener(new MaterialSearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                if (query != null && !query.isEmpty()) {
+                    ArrayList<items> search = new ArrayList<items>();
+
+                    for(items item :items.Ar)
+                        if(item.getDescription().contains(query))
+                            search.add(item);
+
+                    Collections.sort(search, new Comparator<items>() {
+                        @Override
+                        public int compare(items c1, items c2) {
+                            return Double.compare(c1.getPrice(), c2.getPrice());
+                        }
+                    });
+
+                    br(search);
+
+                }else{
+                    br(items.Ar);
+                }
+                return true ;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                if (newText != null && !newText.isEmpty()) {
+                    ArrayList<items> search = new ArrayList<items>();
+
+                    for(items item :items.Ar)
+                        if(item.getDescription().contains(newText))
+                            search.add(item);
+
+                    Collections.sort(search, new Comparator<items>() {
+                        @Override
+                        public int compare(items c1, items c2) {
+                            return Double.compare(c1.getPrice(), c2.getPrice());
+                        }
+                    });
+
+                    br(search);
+
+                }else{
+                    br(items.Ar);
+                }
+                return true ;
+            }
+        });
 
        /* Collections.sort(items.Ar, new Comparator<items>() {
             @Override
@@ -30,7 +112,20 @@ public class browseActivity extends AppCompatActivity  {
 
 
 
-        GridAdapter adapterViewAndroid = new GridAdapter(browseActivity.this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_item, menu); ;
+        MenuItem item = menu.findItem(R.id.action_search) ;
+        searchView.setMenuItem(item);
+        return true ;
+    }
+
+    public void br (ArrayList<items> Ar){
+
+
+        GridAdapter adapterViewAndroid = new GridAdapter(browseActivity.this,Ar);
         androidGridView=(GridView)findViewById(R.id.gridView);
         androidGridView.setAdapter(adapterViewAndroid);
         androidGridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -49,9 +144,5 @@ public class browseActivity extends AppCompatActivity  {
 
 
     }
-
-
-
-
-    }
+}
 
